b9cb688b4237cb354a811aa6db63fecd977e4f47
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2022-07-01 17:48:32 +0100

    Setup the environment variables in .env file

--- I4G005916GDC/I4G005916GDC/settings.py
+++ I4G005916GDC/I4G005916GDC/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rj@8ie3^xlgm*&l%twps1dly^dcj@z%=zhd*ud-n7k_r(*z_qd'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
